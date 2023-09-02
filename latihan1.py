e3e25a3be0d5fe7430c767c632e4367bb15c49fe
Ahmad Dani <98300395+ahmaddani227@users.noreply.github.com>
2023-09-02 10:59:38 +0700

    Add files via upload
    Coding Journey - Python

---
+++ latihan1.py
@@ -0,0 +1,49 @@
+# nama_saya = "Text Ini bertipe"
+# print(nama_saya, "bertipe", type(nama_saya))
+
+# umur = 16
+# print(umur, "bertipe", type(umur))
+
+# IPK = 4.00
+# print(IPK, "bertipe", type(IPK))
+
+# menikah = False
+# print("menikah ?", menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("matkul : ", matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ("web", "etika")
+# print("matkul lain : ", matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = {"alpro", "Alpro", "web", "web", "Data Science", "Data Science"}
+# print("matkul lain : ", matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+# print(5 + 7)
+# print(7 - 5)
+# print(7 / 5)
+# print(5 * 7)
+# print(7 // 5)
+# print(7 % 5)
+
+# universitas = "Universitas Trunojoyo Madura"
+# print(universitas)
+# univ = str(input("Masukkan Nama Unive = "))
+# print(univ)
+
+# angka1 = int(input("Masukkan angka ke 1 = "))
+# angka2 = int(input("Masukkan angka ke 2 = "))
+# print(angka1 + angka2)
+
+# LATIHAN
+# nama_saya = str(input("Masukkan nama Anda = "))
+# print(nama_saya, "bertipe", type(nama_saya))
+
+# umur = int(input("Masukkan umur Anda = "))
+# print(umur, "bertipe", type(umur))
+
+# IPK = float(input("Masukkan nilai IPK anda = "))
+# print(IPK, "bertipe", type(IPK))
+
+# menikah = bool(input("Sudah menikah = "))
+# print("menikah ?", menikah, "bertipe", type(menikah))
